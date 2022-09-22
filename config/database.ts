f2f7dc78c25e601145bc59b144a73f70ce596ca3
Humed-Muhammad <abc45171@gmail.com>
2022-09-22 12:21:13 +0300

    🎨 IMPROVE:

--- config/database.ts
+++ config/database.ts
@@ -2,10 +2,10 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'psql.humedfables.net'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
+      database: env('DATABASE_NAME', 'sweetfoodies'),
+      user: env('DATABASE_USERNAME', 'imagezillow'),
       password: env('DATABASE_PASSWORD', 'hum.'),
       ssl: env.bool('DATABASE_SSL', false),
     },
